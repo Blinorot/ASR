27c6d808155a9c8e406c8d8d9bd10765b76e642d
Blinorot <pmgrin.work@gmail.com>
2022-10-03 22:18:08 +0300

    Beam Search Added

--- hw_asr/metric/__init__.py
+++ hw_asr/metric/__init__.py
@@ -1,7 +1,9 @@
-from hw_asr.metric.cer_metric import ArgmaxCERMetric
-from hw_asr.metric.wer_metric import ArgmaxWERMetric
+from hw_asr.metric.cer_metric import ArgmaxCERMetric, BeamSearchCERMetric
+from hw_asr.metric.wer_metric import ArgmaxWERMetric, BeamSearchWERMetric
 
 __all__ = [
     "ArgmaxWERMetric",
-    "ArgmaxCERMetric"
+    "ArgmaxCERMetric",
+    "BeamSearchCERMetric",
+    "BeamSearchWERMetric",
 ]
--- hw_asr/metric/wer_metric.py
+++ hw_asr/metric/wer_metric.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
 from typing import List
 
+import numpy as np
 import torch
 from hw_asr.base.base_metric import BaseMetric
 from hw_asr.base.base_text_encoder import BaseTextEncoder
@@ -27,3 +29,64 @@ class ArgmaxWERMetric(BaseMetric):
                 pred_text = self.text_encoder.decode(log_prob_vec[:length])
             wers.append(calc_wer(target_text, pred_text))
         return sum(wers) / len(wers)
+
+
+class BeamSearchWERMetric(BaseMetric):
+    """
+    WER Metric when predicted text is beam_search over log_probs
+    """
+    def __init__(self, text_encoder: BaseTextEncoder, beam_size: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.text_encoder = text_encoder
+        self.beam_size = beam_size
+
+    def __call__(self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs):
+        wers = []
+        predictions = log_probs.detach().cpu().numpy()
+        lengths = log_probs_length.detach().numpy()
+        for log_prob_vec, length, target_text in zip(predictions, lengths, text):
+            target_text = BaseTextEncoder.normalize_text(target_text)
+
+            sentence, proba = self._beam_search(log_prob_vec[:length])
+            sentence = np.array(list(sentence[0]))
+
+            if hasattr(self.text_encoder, "ctc_decode"):
+                pred_text = self.text_encoder.ctc_decode(sentence)
+            else:
+                pred_text = self.text_encoder.decode(sentence)
+            wers.append(calc_wer(target_text, pred_text))
+        return sum(wers) / len(wers)
+
+    # based on seminar materials
+    def _beam_search(self, probs):
+        beam = {}
+
+        for prob in probs:
+            beam = self._extend_beam(beam, prob)
+            beam = self._cut_beam(beam)
+        
+        sorted_beam = sorted(beam.items(), key=lambda x: -x[1])
+        return sorted_beam[0]
+
+    def _extend_beam(self, beam, prob):
+        if len(beam) == 0:
+            last_char = prob.argmax() # char in int format
+            beam = {
+                ((last_char,), last_char): prob[last_char].item()
+            }
+            return beam
+
+        new_beam = defaultdict(float)
+        
+        for (sentence, last_char), v in beam.items():
+            for i in range(len(prob)):
+                if i == last_char:
+                    new_beam[(sentence, last_char)] += v * prob[i]
+                else:
+                    new_beam[((*sentence, i), i)] += v * prob[i]
+
+        return new_beam
+
+    def _cut_beam(self, beam):
+        return dict(sorted(beam.items(), key=lambda x: -x[1])[:self.beam_size])
+        
